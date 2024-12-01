364876550120505e31ccaa9cab2e31801dbc3193
SamuelMagalhaes <samuel.signas@gmail.com>
2024-12-01 19:53:37 -0300

    Criação do aviso de erro e número de tentativas

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const randomNumber = Math.round(Math.random() * 10)
 const h2 = document.querySelector('h2')
+
+const verify = document.querySelector('.verify')
+const reload = document.querySelector('.reload')
+
+const warning = document.querySelector('.warning')
+const warningText = document.querySelector('.warning-text')
+const warningTries = document.querySelector('.tries')
+
 let Attempts = 1
 
-function handleClick(event) {
+verify.addEventListener('click', (event) => {
     event.preventDefault()
 
     const inputNumber = document.querySelector("#inputNumber").value
@@ -12,10 +20,18 @@ function handleClick(event) {
         document.querySelector('.screen1').classList.add('hide')
 
         h2.innerText = `Você acertou em ${Attempts} tentativas`
+    } else {
+        warning.classList.remove('hide')
+        warningText.innerText = "Número errado, tente novamente"
+        warningTries.innerText = `Tentativas: ${Attempts}`
+
+        setTimeout(() => {
+            warning.classList.add('hide')
+        }, 1500 )
     }
     Attempts++
-}
+}) 
 
-function windowReaload() { 
-    location.reload
-}
+reload.addEventListener('click', () => {
+    location.reload()
+})
